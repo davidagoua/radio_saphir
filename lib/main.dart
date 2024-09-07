f433d3664c5be0a2b8d7a158737c2296f0024979
root <daniel.ntaye@sahanalytics.com>
2024-09-07 15:55:30 +0000

    enhance ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:get/get.dart';
-import 'package:flutter_uxcam/flutter_uxcam.dart';
+// import 'package:flutter_uxcam/flutter_uxcam.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
 
   WidgetsFlutterBinding.ensureInitialized();
+  /*
   FlutterUxcam.optIntoSchematicRecordings(); // Confirm that you have user permission for screen recording
   FlutterUxConfig config = FlutterUxConfig(
     userAppKey: "r516z16gznlpsca",
     enableAutomaticScreenNameTagging: false);
   FlutterUxcam.startWithConfiguration(config);
+
+   */
   
   runApp(
     GetMaterialApp(
